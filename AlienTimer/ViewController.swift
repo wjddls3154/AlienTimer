53d9351d57badc11c21a22b7d5c23c0981458ed8
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-04-04 15:23:30 +0900

    alienTimer

--- AlienTimer/ViewController.swift
+++ AlienTimer/ViewController.swift
@@ -9,17 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    var counter = 1
+    var myTimer = Timer()
+    var check = true
+    var checked = true
+    //타이머 객체 생성
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var imageCounter: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+              imageCounter.text = String(counter)
+        myImageView.image = UIImage(named: "frame1.png")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func play(_ sender: Any) {
+        if checked == true {
+            
+            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector:#selector(doAnimation) , userInfo: nil, repeats: true)
+            checked = false
+        } else if checked == false{
+            myTimer.invalidate()
+            checked = true
+            
+            
+        }
+        //타이머 실행
     }
+    
 
+    @objc func doAnimation(){
+        if counter == 5 {
+            check = false
+        }else if counter == 1 {
+            check = true
+        }
+        if check == true {
+            counter = counter + 1
+        }else {
+            counter = counter - 1
+        }
+        
+        
+        
+        //타이머에 의해 동적으로 호출되는 함수
+        
+        myImageView.image = UIImage(named: "frame\(counter).png")
+        imageCounter.text = String(counter)
+    }
+    
+    
 
 }
 
